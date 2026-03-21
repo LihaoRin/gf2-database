250ec229199e22faea98f6b4637544dac7aef23c
LihaoRin <kondoushana@gmail.com>
2026-03-22 04:11:04 +0900

    fix: prevent serverless bundle overflow

--- app/characters/[id]/page.tsx
+++ app/characters/[id]/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-static";
+
 import { notFound } from "next/navigation";
 import CharacterDetailPage from "@/components/CharacterDetailPage";
 import characters from "@/data/characters.json";
